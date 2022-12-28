39b91b8380a8863422ec6530995f4f5312e49ec0
Eli Stouraitis <h.stoyraitis@gmail.com>
2022-12-28 15:23:07 +0200

    ADD multithreading support

--- kentia_scraper.py
+++ kentia_scraper.py
@@ -4,6 +4,8 @@ import os
 import openpyxl as xl
 from time import sleep, perf_counter
 import pprint as pp
+import re
+from multiprocessing import Pool
 
 
 
@@ -26,30 +28,27 @@ def getimagecontent(i, url, productcode):
     with open(os.getcwd() + "\\images\\" + str(productcode) + "_" + str(i) + ".jpg", 'wb') as f:
         f.write(requests.get(url).content)
 
-wb = xl.load_workbook('kentia-2022-12-27-diff(2)-v2.xlsx')
-sheet = wb['CMSInput']
-url = 'https://www.kentia.gr/el/product-search?search='
+# wb = xl.load_workbook('kentia-2022-12-27-diff(2)-v2.xlsx')
+# sheet = wb['CMSInput']
 
-length = 1
-while sheet['b'+str(length+1)].value:
-    length += 1
+# length = 1
+# while sheet['b'+str(length+1)].value:
+#     length += 1
 
-start = perf_counter()
+# start = perf_counter()
 
-f = open('log_failed.txt', 'w')
-offset = 2
-while sheet['b'+str(offset)].value:
-    # sleep(2)
-    offset += 1
-    productcode = sheet['b'+str(offset)].value
-    print(f'Downloading item {productcode}, {offset-2}/{length}')
+def app(item):
+    url = 'https://www.kentia.gr/el/product-search?search='
+    f = open('log_failed.txt', 'a')
+    productcode = item
+    print(f'Downloading item {item}')
     if not productcode:
-        continue
+        return
     productlink = getproductlink(url, productcode)
     if not productlink:
         print(f'product link not found, logging {productcode=}')
         f.write(f"{productcode}'\n'")
-        continue
+        return
     imagelinks = getimagelinks(productlink.split('?')[0])
     print(f'Found image links: {len(imagelinks)}')
     if not imagelinks:
@@ -58,5 +57,51 @@ while sheet['b'+str(offset)].value:
     for i, link in enumerate(imagelinks):
         print(f"Saving [{i+1} out of {len(imagelinks)}]", end='\r')
         getimagecontent(i, link, productcode)
-    print(f"Estimated time to completion: {(((perf_counter()-start)/(offset-2))*(length-(offset-2)))//60}")
-f.close()
+    # print(f"Estimated time to completion: {(((perf_counter()-start)/(offset-2))*(length-(offset-2)))//60}")
+    f.close()
+
+
+def import_product_codes() -> list[str]:
+
+    #xlsx filename input & sanitation
+    files = os.listdir()
+    print("Select a file by number")
+    for i, file in enumerate(files):
+        if not re.search(".*\.xls.", file):
+            files[i] = ""
+    
+    while "" in files:
+        files.pop(files.index(""))
+    
+    for i, file in enumerate(files):
+        print(f"{i} - {file}")
+    flag = True
+    while flag:
+        try:
+            filename = files[int(input("> "))]
+            flag = False
+        except Exception as e:
+            if e != KeyboardInterrupt:
+                print(f"Input must be a number between 0 and {len(files)-1}")
+            else: break
+    
+    #creating a list of codes
+    codes = []
+    wb = xl.load_workbook(filename)
+    sheet = wb['CMSInput']
+    offset = 2
+    while sheet[f'b{offset}'].value:
+        codes.append(sheet[f'b{offset}'].value)
+        offset += 1
+
+    return codes
+
+
+def main(threads=4):
+    products = import_product_codes()
+    with Pool(threads) as pool:
+        pool.map(app, products)
+
+
+if __name__ == "__main__":
+    main()
